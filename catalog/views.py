40149d0f765747f30f54b0a9b79e5493c15f4fcc
Nitish Aggarwal <royalnitish21@gmail.com>
2019-12-03 02:17:24 +0530

    Adding author and author details pages

--- catalog/views.py
+++ catalog/views.py
@@ -36,3 +36,12 @@ class BookListView(generic.ListView):
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
